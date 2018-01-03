26eaf916f2eff108f456e8393af94a0d578e2744
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-01-02 22:03:52 -0800

    Faster solution to day 20

--- day20.py
+++ day20.py
@@ -3,17 +3,14 @@
 from itertools import count
 
 def part1(limit):
-    biggest = 0 
-    for house in count(1):
-        total = 0
-        for elf in range(1, int(house**0.5)):
-            if elf * elf == house:
-                total += 10 * elf
-            elif house % elf == 0:
-                total += 10 * elf
-                total += 10 * house // elf
-        if total > limit:
-            return house
+    n_houses = ((limit+1) // 10)
+    houses = [10] * n_houses
+    for elf in range(2, n_houses):
+        for n in range(elf, n_houses, elf):
+            houses[n] += 10 * elf
+    for house, presents in enumerate(houses[1:], 1):
+        if presents > limit:
+            return house 
 
 def part2(limit):
     biggest = 0 
@@ -27,5 +24,7 @@ def part2(limit):
 
 if __name__ == '__main__':
     inp = 36000000
+    # Part 1: 831600
+    # Part 2: 884520
     print("Part 1: {}".format(part1(inp)))
     print("Part 2: {}".format(part2(inp)))
