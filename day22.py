3786d7c47c5f6bae1be1dacfbe3cadaead7571ca
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-01-06 22:52:08 -0800

    Still working on it

--- day22.py
+++ day22.py
@@ -1,101 +1,88 @@
 #!/usr/bin/env python3
 
 from get_input import get_input
-from collections import defaultdict
-from copy import deepcopy
+from copy import copy
 
-class Boss(object):
-    def __init__(self, attack, defence, hp):
-        self.hp = hp
-        self.defence = defence
-        self.attack = attack
-        self.is_player = False
+SPELLS = []
 
-    def clone(self):
-        yield deepcopy(self)
+class SpellException(Exception):
+    pass
 
-class Player(object):
-    def __init__(self, hp=50, mana=500):
-        self._mana = mana
-        self.mana_used = 0
-        self.hp = hp
-        self.is_player = True
-        self._timers = defaultdict(int)
-        self._attack = None
-        self._actions = [self.magic_missle]
+# Magic Missile costs 53 mana. It instantly does 4 damage.
+# Drain costs 73 mana. It instantly does 2 damage and heals you for 2 hit points.
+# Shield costs 113 mana. It starts an effect that lasts for 6 turns. While it is active, your armor is increased by 7.
+# Poison costs 173 mana. It starts an effect that lasts for 6 turns. At the start of each turn while it is active, it deals the boss 3 damage.
+# Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the start of each turn while it is active, it gives you 101 new mana.
+def spell(cost, cooldown=0):
+    def cast_spell(spell):
+        def wrapped(caster):
+            print('Casting {}'.format(spell.__name__))
+            if cost > caster['mana']:
+                raise SpellException('Out of mana')
+            if caster.get(spell.__name__, 0) > 0:
+                raise SpellException('{} Cooldown {}'.format(spell.__name__,
+                    caster[spell.__name__]))
+            caster[spell.__name__] = cooldown
+            caster['mana'] -= cost + 0 if caster['Recharge'] <= 0 else 110
+            caster['mana used'] += cost
+            attack_val = spell(caster)
+            return attack_val + 0 if caster['Poison'] <= 0 else 3
+        return wrapped
+    SPELLS.append(cast_spell)
+    return cast_spell
 
-    @property
-    def mana(self):
-        return self._mana
+@spell(53)
+def magic_missle(caster):
+    return 4
 
-    @mana.setter
-    def mana(self, value):
-        if value < 0:
-            raise ValueError("Not enough mana {}".format(value))
-        self.mana_used += max(self._mana - value, 0)
-        self._mana = value
+@spell(73)
+def drain(caster):
+    caster['Hip Points'] += 2
+    return 2
 
-    @property
-    def defence(self):
-        return 0
-    
-    @property
-    def attack(self):
-        return 0 
+@spell(113, cooldown=6)
+def Shield(caster):
+    return 0
 
-    def magic_missle(self):
-        self.mana -= 53
-        return 4
+@spell(173, cooldown=6)
+def Poison(caster):
+    return 0
 
-    def drain(self):
-        self.mana -= 73
-        self.hp += 2
-        return 2
-
-    @self.timer(6)
-    def shield(self):
-        self.mana -= 113
-        return 0
-
-    @self.timer(6)
-    def poison(self):
-        self.mana -= 173
-        return 3
-
-    @self.timer(5)
-    def recharge(self):
-        self.mana -= 229
-        return 0
-
-    def clone(self):
-        for action in self._actions: 
-            copy = deepcopy(self)
-            copy.timers = {k: max(v-1, 0) for k, v in copy._timers.items()}
-            copy._attack = action
-            yield copy
+@spell(229, cooldown=5)
+def Recharge(caster):
+    return 0
 
 def part1(player, boss):
-    queue = [(player, boss)]
     min_mana = None
+    queue = [(player, boss)]
     while queue:
-        attacker, defender = queue.pop()
-        defender.hp -= max(attacker.attack - defender.defence, 1)
-        if defender.hp <= 0 and not defender.is_player:
-            min_mana = min(min_mana or defender.mana_used, defender.mana_used)
-        elif 0 < defender.hp:
-            for clone in attacker.clone():
-                queue.append((defender, clone))
+        player, boss = queue.pop()
+        for spell in SPELLS:
+            clone_p, clone_b = copy(player), copy(boss)
+            print(spell)
+            try:
+                clone_b['Hit Points'] -= spell(clone_p)
+            except SpellException as e:
+                print("Oops {}".format(e))
+                continue
+            if clone_b.hp <= 0:
+                min_mana = min(min_mana or clone_p['mana used'], clone_p['mana used'])
+            defence = 7 if clone_p.get('Shield',0) > 0 else 0
+            clone_p.hp -= max(1, clone_b['Damage'] - defence)
+            if clone_p.hp > 0:
+                queue.append((clone_p, clone_b))
     return min_mana
 
 def part2(player, boss):
     pass
 
 def parse(text):
-    boss_stats = dict(line.split(': ') for line in text.splitlines())
-    boss = Boss(defence=0,
-            attack=int(boss_stats['Damage']),
-            hp=int(boss_stats['Hit Points']))
-    return boss, Player()
+    boss = {'Hit Points': None, 'Attack': None}
+    for line in text.splitlines():
+        vals = line.split(': ')
+        if vals[0] in boss:
+            boss[vals[0]] = int(vals[1])
+    return boss, {'mana': 500, 'Hit Points': 50}
 
 if __name__ == '__main__':
     boss, player = parse(get_input(day=22, year=2015))
