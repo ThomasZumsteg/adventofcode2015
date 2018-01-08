a31ad632d441304f7587ff2c189abe0dbb2560f2
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-01-07 22:18:25 -0800

    Solution to day 22 part 1

--- day22.py
+++ day22.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from get_input import get_input
-from copy import copy
+from collections import defaultdict
+from functools import wraps
+from copy import deepcopy
 
 SPELLS = []
 
@@ -15,21 +17,26 @@ class SpellException(Exception):
 # Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the start of each turn while it is active, it gives you 101 new mana.
 def spell(cost, cooldown=0):
     def cast_spell(spell):
+        @wraps(spell)
         def wrapped(caster):
-            print('Casting {}'.format(spell.__name__))
             if cost > caster['mana']:
                 raise SpellException('Out of mana')
-            if caster.get(spell.__name__, 0) > 0:
+            if caster[spell.__name__] > 0:
                 raise SpellException('{} Cooldown {}'.format(spell.__name__,
                     caster[spell.__name__]))
-            caster[spell.__name__] = cooldown
-            caster['mana'] -= cost + 0 if caster['Recharge'] <= 0 else 110
+            caster[spell.__name__] = cooldown + 1
+            caster['mana'] -= cost
             caster['mana used'] += cost
-            attack_val = spell(caster)
-            return attack_val + 0 if caster['Poison'] <= 0 else 3
+            caster['spells'].append(spell.__name__)
+            tick(caster)
+            return spell(caster)
+        SPELLS.append(wrapped)
         return wrapped
-    SPELLS.append(cast_spell)
     return cast_spell
+
+def tick(caster):
+    for s in SPELLS:
+        caster[s.__name__] -= 1
 
 @spell(53)
 def magic_missle(caster):
@@ -37,54 +44,128 @@ def magic_missle(caster):
 
 @spell(73)
 def drain(caster):
-    caster['Hip Points'] += 2
+    caster['Hit Points'] += 2
     return 2
 
 @spell(113, cooldown=6)
-def Shield(caster):
+def shield(caster):
     return 0
 
 @spell(173, cooldown=6)
-def Poison(caster):
+def poison(caster):
     return 0
 
 @spell(229, cooldown=5)
-def Recharge(caster):
+def recharge(caster):
     return 0
+
+def worth_it(player, boss, min_mana):
+    if min_mana is None:
+        return True
+    if player['Hit Points'] <= 0 or boss['Hit Points'] <= 0:
+        return False
+    if boss['player']:
+        player, boss = boss, player
+    return player['mana used'] < min_mana
 
 def part1(player, boss):
     min_mana = None
     queue = [(player, boss)]
     while queue:
-        player, boss = queue.pop()
-        for spell in SPELLS:
-            clone_p, clone_b = copy(player), copy(boss)
-            print(spell)
+        attacker, defender = queue.pop()
+        for attack in attacker['attacks']:
+            a, d = deepcopy(attacker), deepcopy(defender)
+            defender_is_player = d['player']
+
+            defence = 0
+            if defender_is_player:
+                defence = 7 if d['shield'] > 0 else 0
+                d['mana'] += 0 if d['recharge'] <= 0 else 101
+                a['Hit Points'] -= 0 if d['poison'] <= 0 else 3
+                tick(d)
+            else:
+                a['mana'] += 0 if a['recharge'] <= 0 else 101
+                d['Hit Points'] -= 0 if a['poison'] <= 0 else 3
+
             try:
-                clone_b['Hit Points'] -= spell(clone_p)
+                d['Hit Points'] -= max(1 if defender_is_player else 0,
+                        attack(a) - defence)
             except SpellException as e:
-                print("Oops {}".format(e))
                 continue
-            if clone_b.hp <= 0:
-                min_mana = min(min_mana or clone_p['mana used'], clone_p['mana used'])
-            defence = 7 if clone_p.get('Shield',0) > 0 else 0
-            clone_p.hp -= max(1, clone_b['Damage'] - defence)
-            if clone_p.hp > 0:
-                queue.append((clone_p, clone_b))
+
+            if d['Hit Points'] <= 0 and not defender_is_player:
+                min_mana = min(min_mana or a['mana used'], a['mana used'])
+            if d['Hit Points'] > 0 and worth_it(a, d, min_mana):
+                x, y = a, d
+                if defender_is_player:
+                    y, x = x, y
+                # print('{} {:4} Boss: {:2} Player: {:2} {:3} {}'.format(
+                #     'B' if defender_is_player else 'P', min_mana, y['Hit Points'],
+                #     x['Hit Points'], x['mana'],','.join(x['spells'])))
+                queue.append((d, a))
     return min_mana
 
+def test_part1():
+    player = defaultdict(int, 
+            {'mana': 500, 'Hit Points': 50, 'mana used': 0, 'spells': []})
+    boss = {"Hit Points": 13, 'Damage': 8}
+    assert 3 == poison(player)
+
 def part2(player, boss):
-    pass
+    min_mana = None
+    queue = [(player, boss)]
+    while queue:
+        attacker, defender = queue.pop()
+        for attack in attacker['attacks']:
+            a, d = deepcopy(attacker), deepcopy(defender)
+            defender_is_player = d['player']
+
+            defence = 0
+            if defender_is_player:
+                defence = 7 if d['shield'] > 0 else 0
+                d['mana'] += 0 if d['recharge'] <= 0 else 101
+                a['Hit Points'] -= 0 if d['poison'] <= 0 else 3
+                tick(d)
+            else:
+                a['Hit Points'] -= 1
+                a['mana'] += 0 if a['recharge'] <= 0 else 101
+                d['Hit Points'] -= 0 if a['poison'] <= 0 else 3
+
+            try:
+                d['Hit Points'] -= max(1 if defender_is_player else 0,
+                        attack(a) - defence)
+            except SpellException as e:
+                continue
+
+            if (d['Hit Points'] <= 0 and not defender_is_player) or \
+                (a['Hit Points'] <= 0 and defender_is_player):
+                if defender_is_player:
+                    a = d
+                min_mana = min(min_mana or a['mana used'], a['mana used'])
+            if worth_it(a, d, min_mana):
+                x, y = a, d
+                if defender_is_player:
+                    y, x = x, y
+                print('{} {:4} Boss: {:2} Player: {:2} {:3} {}'.format(
+                    'B' if defender_is_player else 'P', min_mana, y['Hit Points'],
+                    x['Hit Points'], x['mana'],','.join(x['spells'])))
+                queue.append((d, a))
+    return min_mana
 
 def parse(text):
-    boss = {'Hit Points': None, 'Attack': None}
+    boss = {'Hit Points': None, 'Damage': None}
     for line in text.splitlines():
-        vals = line.split(': ')
-        if vals[0] in boss:
-            boss[vals[0]] = int(vals[1])
-    return boss, {'mana': 500, 'Hit Points': 50}
+        if line == '':
+            continue
+        k, v = line.split(': ')
+        boss[k] = int(v)
+    boss['attacks'] = [lambda _: boss['Damage']]
+    boss['player'] = False
+    player = {'mana': 500, 'Hit Points': 50, 'mana used': 0, 'spells': [],
+            'attacks': SPELLS, 'player': True}
+    return boss, defaultdict(int, player)
 
 if __name__ == '__main__':
     boss, player = parse(get_input(day=22, year=2015))
-    print("Part 1: {}".format(part1(player, boss)))
+    # print("Part 1: {}".format(part1(player, boss)))
     print("Part 2: {}".format(part2(player, boss)))
